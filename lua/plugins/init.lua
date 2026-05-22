79402dfdd2baff852c889edaf7991d81b7fafe67
yerdzxc <yerdzxc@users.noreply.github.com>
2026-05-22 10:46:54 +0000

    nvim-tree: show dotfiles (.env, .gitignore, etc.)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,29 @@ return {
     end,
   },
 
+  -- OSC 52 clipboard for SSH (WezTerm)
+  {
+    "ojroques/nvim-osc52",
+    event = "VeryLazy",
+    config = function()
+      require("osc52").setup()
+      local function copy()
+        if vim.v.event.operator == "y" then
+          require("osc52").copy_register("+")
+        end
+      end
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+    end,
+  },
+
+  -- Show dotfiles (.env, etc.) in file explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      filters = { dotfiles = false },
+    },
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
